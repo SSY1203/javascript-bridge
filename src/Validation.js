a5676f6a319fa8e0c89cace0ddf4ad8eae5a91d5
SSY1203 <tlstjsdud566@naver.com>
2022-11-21 20:48:22 +0900

    feat : 다리 길이에 대한 예외 처리2

--- src/Validation.js
+++ src/Validation.js
@@ -3,6 +3,9 @@ class Validation {
     if (size < 3 || size > 20) {
       throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
     }
+    if (isNaN(size)) {
+      throw Error('[ERROR] 숫자를 입력해주세요.');
+    }
     return;
   }
 }
