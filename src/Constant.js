db8df80af2f79bf4e7d5be27fefc234b614e7030
SSY1203 <tlstjsdud566@naver.com>
2022-11-21 19:00:44 +0900

    feat : 다리 길이 입력 받고 랜덤 다리 생성

--- src/Constant.js
+++ src/Constant.js
@@ -3,5 +3,6 @@ const Console = MissionUtils.Console;
 const GAME_START = '다리 건너기 게임을 시작합니다.';
 const PLZ_BRIDGE_LENGTH = '다리의 길이를 입력해주세요.';
 
-exports.GAME_START = GAME_START;
 exports.Console = Console;
+exports.GAME_START = GAME_START;
+exports.PLZ_BRIDGE_LENGTH = PLZ_BRIDGE_LENGTH;
--- src/InputView.js
+++ src/InputView.js
@@ -1,20 +1,15 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+const BridgeGame = require('./BridgeGame');
+const { Console, PLZ_BRIDGE_LENGTH } = require('./Constant');
+const bridge = new BridgeGame();
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize() {
+    Console.readLine(`${PLZ_BRIDGE_LENGTH}\n`, size => {
+      bridge.setBridge(size);
+    });
+  },
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
   readMoving() {},
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
   readGameCommand() {},
 };
 
