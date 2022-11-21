58bb3d91ada37a8c27b0869d1573664d2e82fa76
SSY1203 <tlstjsdud566@naver.com>
2022-11-22 00:18:31 +0900

    refactor : 리팩토링 과정2

--- src/Validation.js
+++ src/Validation.js
@@ -1,23 +1,24 @@
-class Validation {
+const Validation = {
   bridgeSizeCheck(size) {
     if (size < 3 || size > 20 || isNaN(size)) {
       throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
     }
-    return;
-  }
+    return true;
+  },
 
   moveBridgePositionCheck(position) {
     if (position !== 'U' && position !== 'D') {
       throw Error('[ERROR] U 또는 D를 입력해주세요.');
     }
-    return;
-  }
+    return true;
+  },
 
   retryOrQuitInputCheck(selection) {
     if (selection !== 'R' && selection !== 'Q') {
       throw Error('[ERROR] R 또는 Q를 입력해주세요.');
     }
-  }
-}
+    return true;
+  },
+};
 
 module.exports = Validation;
