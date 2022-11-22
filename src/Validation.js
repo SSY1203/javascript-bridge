2de8e14ce47f863551ee749145e104c3ae4f9dd5
SSY1203 <tlstjsdud566@naver.com>
2022-11-22 11:30:50 +0900

    예외 처리 후 그 부분부터 다시 실행

--- src/Validation.js
+++ src/Validation.js
@@ -1,23 +1,37 @@
+const { Console } = require('./Constant');
+
 const Validation = {
   bridgeSizeCheck(size) {
-    if (size < 3 || size > 20 || isNaN(size)) {
-      throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    try {
+      if (size < 3 || size > 20 || isNaN(size)) {
+        throw Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+      }
+    } catch (e) {
+      Console.print(e);
+      return true;
     }
-    return true;
   },
 
   moveBridgePositionCheck(position) {
-    if (position !== 'U' && position !== 'D') {
-      throw Error('[ERROR] U 또는 D를 입력해주세요.');
+    try {
+      if (position !== 'U' && position !== 'D') {
+        throw Error('[ERROR] U 또는 D를 입력해주세요.');
+      }
+    } catch (e) {
+      Console.print(e);
+      return true;
     }
-    return true;
   },
 
   retryOrQuitInputCheck(selection) {
-    if (selection !== 'R' && selection !== 'Q') {
-      throw Error('[ERROR] R 또는 Q를 입력해주세요.');
+    try {
+      if (selection !== 'R' && selection !== 'Q') {
+        throw Error('[ERROR] R 또는 Q를 입력해주세요.');
+      }
+    } catch (e) {
+      Console.print(e);
+      return true;
     }
-    return true;
   },
 };
 
