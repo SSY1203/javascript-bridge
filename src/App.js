63c1a105ebc8747c7834e07f21c3809f36b0fcb6
SSY1203 <tlstjsdud566@naver.com>
2022-11-22 12:06:10 +0900

    refactor : App 파일 메소드가 한가지 일만 하게 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const OutputView = require('./OutputView');
 const InputView = require('./InputView');
 class App {
   play() {
+    this.gameStart();
+  }
+
+  gameStart() {
     OutputView.printStart();
     InputView.readBridgeSize();
   }
